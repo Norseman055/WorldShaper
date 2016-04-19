64b05d562bd90cd2b3f251d271dedea299520e5c
Connor Mahoney <connor.mahoney@metova.com>
2016-04-19 01:22:57 -0500

    Render: - Refactor gameObject to prep for rendering

--- GameObjectManager/GameObject.cpp
+++ GameObjectManager/GameObject.cpp
@@ -3,6 +3,7 @@
 
 #include "Math\Matrix.h"
 
+#include "Vertex.h"
 #include "Shader.h"
 #include "Texture.h"
 #include "Model.h"
@@ -44,6 +45,7 @@ void GameObject::update( const double gameTime ) const {
 
 void GameObject::draw(Camera* const camera) const {
 	GLuint shaderProgram = this->shader->getShaderID();
+	GLuint textureID = this->texture->getTextureID();
 
 	Matrix viewMatrix(camera->getViewMatrix());
 	Matrix projectionMatrix(camera->getProjectionMatrix());
@@ -63,7 +65,7 @@ void GameObject::draw(Camera* const camera) const {
 
 	auto cameraPosLoc = glGetUniformLocation(shaderProgram, "cameraPos");
 
-	glUniform4f(cameraPosLoc, camera->getPosition()[0], camera->getPosition()[1], camera->getPosition()[2], camera->getPosition()[3]);
+	glUniform4f(cameraPosLoc, camera->getPosition()[x], camera->getPosition()[y], camera->getPosition()[z], camera->getPosition()[w]);
 
 	auto specColorLoc = glGetUniformLocation(shaderProgram, "specularColor");
 	auto shinyLoc = glGetUniformLocation(shaderProgram, "shininess");
@@ -79,22 +81,22 @@ void GameObject::draw(Camera* const camera) const {
 	auto vertNormalLoc = glGetAttribLocation(shaderProgram, "vertexNormal");
 	auto uvLoc = glGetAttribLocation(shaderProgram, "UV");
 
-	glBindBuffer(GL_ARRAY_BUFFER, this->vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, this->model->getVboVerts());
 
-	glVertexAttribPointer(vertPosLoc, 4, GL_FLOAT, false, sizeof(vert), static_cast< void * >(nullptr));
+	glVertexAttribPointer(vertPosLoc, 4, GL_FLOAT, false, sizeof(Vertex), static_cast< void * >(nullptr));
 	glEnableVertexAttribArray(vertPosLoc);
 
-	glVertexAttribPointer(vertColorLoc, 4, GL_FLOAT, false, sizeof(vert), reinterpret_cast< void * >(sizeof(point)));
-	glEnableVertexAttribArray(vertColorLoc);
-
-	glVertexAttribPointer(vertNormalLoc, 4, GL_FLOAT, false, sizeof(vert), reinterpret_cast< void * >(2 * sizeof(point)));
+	glVertexAttribPointer(vertNormalLoc, 4, GL_FLOAT, false, sizeof(Vertex), reinterpret_cast< void * >(sizeof(Vect)));
 	glEnableVertexAttribArray(vertNormalLoc);
 
-	glVertexAttribPointer(uvLoc, 4, GL_FLOAT, false, sizeof(vert), reinterpret_cast< void * >(2 * sizeof(point) + sizeof(vector)));
+	glVertexAttribPointer(vertColorLoc, 4, GL_FLOAT, false, sizeof(Vertex), reinterpret_cast< void * >(2 * sizeof(Vect)));
+	glEnableVertexAttribArray(vertColorLoc);
+
+	glVertexAttribPointer(uvLoc, 4, GL_FLOAT, false, sizeof(Vertex), reinterpret_cast< void * >(3 * sizeof(Vect)));
 	glEnableVertexAttribArray(uvLoc);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->index_buffer);
-	glDrawElements(GL_TRIANGLES, this->p_model->getNumFaces(), GL_UNSIGNED_INT, nullptr);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->model->getVboFaces());
+	glDrawElements(GL_TRIANGLES, this->model->getNumFaces(), GL_UNSIGNED_INT, nullptr);
 }
 
 GameObject::GameObject( const GameObjectType inType, const char* inName )
